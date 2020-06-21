acc288c2ba7cacacc64f0ae4e0844fc89d2afece
Tristan Angieri <tristan.angieri@gmail.com>
2020-06-21 18:02:19 +0000

    added instructions to large_power.py

--- exercises_python/large_power.py
+++ exercises_python/large_power.py
@@ -1,3 +1,6 @@
+# Create a function named large_power() that takes two parameters named base and exponent.
+# If base raised to the exponent is greater than 5000, return True, otherwise return False
+
 def large_power(b, e):
   if b**e > 5000:
     return True
