8662c99d8c6596ee9c3e5ea82ad3fdeac3be704d
Tristan Angieri <tristan.angieri@gmail.com>
2020-06-22 22:28:14 +0000

    added larger_list.py

---
+++ exercises_python/lists/larger_list.py
@@ -0,0 +1,7 @@
+# Write a function named larger_list that has two parameters named lst1 and lst2.
+
+# The function should return the last element of the list that contains more elements. If both lists are the same size, then return the last element of lst1.
+
+def larger_list(lst1, lst2):
+  return lst2[-1] if len(lst2) > len(lst1) else lst1[-1]
+
