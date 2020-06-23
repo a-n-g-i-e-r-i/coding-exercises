9bba5ce2488831bf458535773b6fefb14e2ce5ed
Tristan Angieri <tristan.angieri@gmail.com>
2020-06-23 18:11:04 +0000

    added combine_sort.py

---
+++ exercises_python/lists/combine_sort.py
@@ -0,0 +1,9 @@
+# Write a function named combine_sort that has two parameters named lst1 and lst2.
+
+# The function should combine these two lists into one new list and sort the result. Return the new sorted list.
+
+def combine_sort(lst1, lst2):
+  return sorted(lst1 + lst2)
+
+# Should return [-10, 2, 2, 4, 5, 5, 10, 10]
+print(combine_sort([4, 10, 2, 5], [-10, 2, 5, 10]))
